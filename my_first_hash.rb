91098ccbf4b6221a8329669eb64eced43096c6a5
Nadia Lee <miss.nadia.lee@gmail.com>
2017-12-18 03:56:51 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -38,5 +38,8 @@ def adding
   # add 4 gun powder to the shipping_manifest hash below
 
   # return the shipping_manifest hash below
-
 end
+
+shipping_manifest["muskets"] = 2
+shipping_manifest["gun powder"] = 4
+puts shipping_manifest
